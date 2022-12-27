cf8e42277d97c3d5ef461ef5ca1d34cead59ac87
Bolbolito <68690729+Bolbolito@users.noreply.github.com>
2022-12-27 20:13:33 +0800

    linting and update

--- sv_main.lua
+++ sv_main.lua
@@ -1,14 +1,8 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateUseableItem("police_car_tracker", function(source, item)
-    local _source = source
-	TriggerClientEvent('be-policegps:placegps', _source)
-end)
-
-RegisterServerEvent('be-policegps:removeitem')
-AddEventHandler('be-policegps:removeitem', function()
 	local _source = source
-    local Player = QBCore.Functions.GetPlayer(_source)
+	TriggerClientEvent('be-policegps:placegps', _source)
 	Player.Functions.RemoveItem("police_car_tracker", 1)
 	TriggerClientEvent('inventory:client:ItemBox', _source, QBCore.Shared.Items['police_car_tracker'], "remove")
 end)
