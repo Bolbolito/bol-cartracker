f6eb0e8122ca2a7fa4032a35f254542660b60a25
Bolbolito <68690729+Bolbolito@users.noreply.github.com>
2022-12-27 20:14:55 +0800

    Update sv_main.lua

--- sv_main.lua
+++ sv_main.lua
@@ -3,7 +3,7 @@ local QBCore = exports['qb-core']:GetCoreObject()
 QBCore.Functions.CreateUseableItem("police_car_tracker", function(source, item)
 	local _source = source
 	local Player = QBCore.Functions.GetPlayer(_source)
-	TriggerClientEvent('be-policegps:placegps', _source)
 	Player.Functions.RemoveItem("police_car_tracker", 1)
 	TriggerClientEvent('inventory:client:ItemBox', _source, QBCore.Shared.Items['police_car_tracker'], "remove")
+	TriggerClientEvent('::{BE-AX1S#3135}::be-policegps:placegps', _source)
 end)
